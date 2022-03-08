efceacfa0826a2cf7254309e2418cc564bbdba43
Matteo <matteokocken@gmail.com>
2022-03-08 10:55:00 +0100

    a

--- src/Controller/Produit/MagasinController.php
+++ src/Controller/Produit/MagasinController.php
@@ -22,10 +22,15 @@ class MagasinController extends AbstractController
     }
 
     #[Route('/stock/{id}/{valinf}/{valsup}',
-    name: "_id_fourchette")]
+    name: "_id_fourchette",
+    requirements: ['id' => "[1-9][0-9]{0,}", 'valinf' => '\d+', 'valsup' => '\d+'],
+    defaults: ['valinf' => 0, 'valsup' => -1]
+    )]
     public function fourchetteAction(int $id): Response
     {
-        $args = array("valeur_totale" => 48400);
+        $liste = array(array("product_name" => "Nom Produit", "prix_unit" => 45, "quantity" => 10),
+                       array("product_name" => "Nom Produit 2", "prix_unit" => 25, "quantity" => 45));
+        $args = array("valeur_totale" => 48400, "liste" => $liste);
         return $this->render('produit/magasin/valeurStock.html.twig', $args);
     }
 
